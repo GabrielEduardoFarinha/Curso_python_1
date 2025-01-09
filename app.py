179e09c146e4e8ca1d55016c67593d94eae57fe7
Gabriel Eduardo Farinha <94718937+GabrielEduardoFarinha@users.noreply.github.com>
2025-01-09 15:37:33 -0300

    Commit inicial
    Commit contendo o primeiro menu do programa

---
+++ app.py
@@ -0,0 +1,11 @@
+print('Ｓａｂｏｒ Ｅｘｐｒｅｓｓ\n')
+
+
+print('1. cadastrar restaurante')
+print('2. listar restaurantes')
+print('3. ativar restaurante')
+print('4. sair do cadastro\n')
+
+
+opcao_escolhida = input('escolha uma opção: ')
+print(f'você escolhar a opção{opcao_escolhida}')
