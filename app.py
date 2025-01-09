a834c29718eb8f8d50f2c2de0ce638f7b90ad097
Gabriel Eduardo Farinha <94718937+GabrielEduardoFarinha@users.noreply.github.com>
2025-01-09 16:43:08 -0300

    desafios da aula 2
    branch contendo a resolução dos desafios ( ler READ.ME)

--- app.py
+++ app.py
@@ -1,13 +1,32 @@
-print('Python na Escola de Programação da Alura.\n')
+numero = input('digite um numero pf: ')
 
-NOME = 'Gabriel'
-IDADE = '24'
+ultimo_numero = numero [-1]
 
-print(f'Meu nome é {NOME} e tenho {IDADE} anos de vida\n')
+if ultimo_numero in '02468':
+    print(f'o número {numero} é par!')
+else:
+    print(f'o número {numero} é impar')
 
-print('\033[31m' + 'A' + '\033[0m')
-print('L')
-print('U')
-print('R')
-print('\033[31m' + 'A' + '\033[0m\n')
+idade = int(input('digite a sua idade: '))
 
+if idade <= 12:
+    print('vocé é considerado uma criança')
+elif 13 <= idade <= 18:
+    print('vocé é considerado um adolescente')
+else:
+    print('vocé já é um adulto')
+
+usuario_esperado = 123
+senha_esperada = 123
+usuario = int(input('pf digite um usuario: '))
+senha = int(input('pf digite uma senha: '))
+
+if usuario == usuario_esperado:
+    print('usuario correcto')
+else:
+    print('usuario incorreto')
+
+if senha == senha_esperada:
+    print('senha correta')
+else:
+    print('senha incorreta')
