e6b715daeec45799b53e5e0f9bc8c4a86532f7ca
Gabriel Eduardo Farinha <94718937+GabrielEduardoFarinha@users.noreply.github.com>
2025-01-09 16:45:57 -0300

    restruturação do programa usando DEF
    Commit encapsulando o programa em funções

--- app.py
+++ app.py
@@ -1,25 +1,37 @@
 import os
 
-print('Ｓａｂｏｒ Ｅｘｐｒｅｓｓ\n')
+def exibir_nome_do_programa():
+    print('Ｓａｂｏｒ Ｅｘｐｒｅｓｓ\n')
+
+def menu_opcoes():
+    print('1. cadastrar restaurante')
+    print('2. listar restaurantes')
+    print('3. ativar restaurante')
+    print('4. sair do cadastro\n')
+
+def escolha_opcao():
+    opcao_escolhida = int(input('escolha uma opção: '))
 
 
-print('1. cadastrar restaurante')
-print('2. listar restaurantes')
-print('3. ativar restaurante')
-print('4. sair do cadastro\n')
 
-
-opcao_escolhida = int(input('escolha uma opção: '))
+    if opcao_escolhida == 1:
+        print('cadastrar restaurante')
+    elif opcao_escolhida == 2:
+        print('listar restaurantes')
+    elif opcao_escolhida == 3:
+        print('ativar restaurante')
+    else:
+        finalizar_app()
 
 def finalizar_app():
     os.system('cls')
     print('finalizando o app')
 
-if opcao_escolhida == 1:
-    print('cadastrar restaurante')
-elif opcao_escolhida == 2:
-    print('listar restaurantes')
-elif opcao_escolhida == 3:
-    print('ativar restaurante')
-else:
-    finalizar_app()
+def main():
+    exibir_nome_do_programa()
+    menu_opcoes()
+    escolha_opcao()
+
+    
+if __name__ == '__main__':
+    main()
