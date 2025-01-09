26f64e2614222376737ff3528161e5f45e4b21f8
Gabriel Eduardo Farinha <94718937+GabrielEduardoFarinha@users.noreply.github.com>
2025-01-09 16:07:51 -0300

    Adição da função finalizar
    criada a função que finaliza o programa

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 print('Ｓａｂｏｒ Ｅｘｐｒｅｓｓ\n')
 
 
@@ -7,5 +9,17 @@ print('3. ativar restaurante')
 print('4. sair do cadastro\n')
 
 
-opcao_escolhida = input('escolha uma opção: ')
-print(f'você escolhar a opção{opcao_escolhida}')
+opcao_escolhida = int(input('escolha uma opção: '))
+
+def finalizar_app():
+    os.system('cls')
+    print('finalizando o app')
+
+if opcao_escolhida == 1:
+    print('cadastrar restaurante')
+elif opcao_escolhida == 2:
+    print('listar restaurantes')
+elif opcao_escolhida == 3:
+    print('ativar restaurante')
+else:
+    finalizar_app()
